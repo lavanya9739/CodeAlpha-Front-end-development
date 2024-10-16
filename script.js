dc6617be4cddbe36afd5770077f1049b5cc973cf
lavanya9739 <150366732+lavanya9739@users.noreply.github.com>
2024-10-16 12:53:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,61 @@
 let isPreviewOpen = false;
+let currentIndex = 0;
+let galleryImages = []; 
+
+window.onload = function() {
+    galleryImages = Array.from(document.querySelectorAll('.gallery-image'));
+};
+
 function showImageDetails(imgElement) {
     const previewSection = document.getElementById("imagePreview");
+
+    currentIndex = galleryImages.indexOf(imgElement); // Get index of clicked image
+
     if (isPreviewOpen) {
         closePreview();
         return;
     }
-    const imgSrc = imgElement.src;
+
+    updatePreviewImage(); 
+
+    previewSection.style.display = 'block'; // Display preview
+    isPreviewOpen = true;
+}
+
+function updatePreviewImage() {
+    const imgSrc = galleryImages[currentIndex].src;
+    const imgName = galleryImages[currentIndex].getAttribute("data-name"); // Get the data-name attribute
     const previewImg = document.getElementById("previewImg");
     const imageInfo = document.getElementById("imageInfo");
     const downloadLink = document.getElementById("downloadLink");
     const imageNameDisplay = document.getElementById("imageName");
-    const imageName = imgElement.getAttribute("data-name");
-    previewImg.src = imgSrc;
-    imageNameDisplay.innerHTML = `Name: ${imageName}`; 
 
+
+    previewImg.src = imgSrc;
+    imageNameDisplay.innerHTML = `Name: ${imgName}`;
+
+    const fileName = imgSrc.substring(imgSrc.lastIndexOf('/') + 1);
     const image = new Image();
     image.src = imgSrc;
     image.onload = function() {
         const dimensions = `${image.width} x ${image.height}px`;
-        const fileName = imgSrc.substring(imgSrc.lastIndexOf('/') + 1);
-        imageInfo.innerHTML = `File: ${fileName}<br>Dimensions: ${dimensions}`;
-    }
+        imageInfo.innerHTML = `File: ${fileName} <br> Dimensions: ${dimensions}`;
+    };
     downloadLink.href = imgSrc;
-    downloadLink.download = imgSrc.substring(imgSrc.lastIndexOf('/') + 1);
-    previewSection.style.display = 'block';
-    isPreviewOpen = true;
+    downloadLink.download = fileName;
 }
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % galleryImages.length; // Loop to the start if at the last image
+    updatePreviewImage();
+}
+
+
+function prevImage() {
+    currentIndex = (currentIndex - 1 + galleryImages.length) % galleryImages.length; // Loop to the end if at the first image
+    updatePreviewImage();
+}
+
 function closePreview() {
     document.getElementById("imagePreview").style.display = 'none';
     isPreviewOpen = false;
